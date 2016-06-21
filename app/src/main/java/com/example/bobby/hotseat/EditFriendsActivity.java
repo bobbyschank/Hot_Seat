37c7c9757af5e3a99011d0f2699f3e1a8c2e4c58
Robert Schank <robertschank@gmail.com>
2016-06-21 15:50:56 -0600

    test

--- app/src/main/java/com/example/bobby/hotseat/EditFriendsActivity.java
+++ app/src/main/java/com/example/bobby/hotseat/EditFriendsActivity.java
@@ -1,13 +1,27 @@
 package com.example.bobby.hotseat;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.List;
+
 public class EditFriendsActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    protected List<HotSeatUser> mHotSeatUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,20 +29,49 @@ public class EditFriendsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit_friends);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        // setupToolbar();
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    /*
+    private void setupToolbar() {
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        // Query for friends in firebase, order by ascending, setLimit, findInBackground.
+
+        // mHotSeatUsers = hotSeatUsers;
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(EditFriendsActivity.this);
+        builder.setMessage(R.string.error_message)
+                .setTitle(R.string.error_title)
+                .setPositiveButton(android.R.string.ok, null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
 }
